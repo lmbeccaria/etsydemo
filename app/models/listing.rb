8ddebacc28720438af26ae2eb8792b55a146bc5b
lmbeccaria <luis.m.beccaria@gmail.com>
2014-07-23 17:03:41 -0400

    Install and setup Devise

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,7 +3,7 @@ class Listing < ActiveRecord::Base
     has_attached_file :image, :styles => { :large => "400x400", :medium => "275x275>", :small => "100x100>", :thumb => "50x50>" }, :default_url => "default_:style.jpg"
 
   else
-    has_attached_file :image, :styles => {  :large => "400x400", 
+    has_attached_file :image, :styles => {  :large => "400x400>", 
                                             :medium => "275x275>", 
                                             :small => "100x100>", 
                                             :thumb => "50x50>" }, 
